73af3d5514948b0027d2677770130ff1a5c5b6a7
Marcell <38266625+0silverback0@users.noreply.github.com>
2020-06-11 07:45:22 -0400

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,29 @@
+//get contents from .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+// require aylien and get env variables
+const aylien = require("aylien_textapi");
+var textapi = new aylien({
+application_id: process.env.API_ID,
+application_key: process.env.API_KEY
+});
+
+console.log(process.env.API_KEY);
+
+//dependencies
+
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.static('dist'))
 
@@ -20,3 +41,17 @@ app.listen(8081, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+
+const getData = async(req, res) => {
+	 res = await fetch(`https://api.aylien.com/api/v1/sentiment ${process.env.API_KEY}`)
+	try{
+		const data = await res.json();
+		console.log(data)
+		return data;
+	} catch(error) {
+		console.log("error at getData", error);
+	}
+}
+
+app.get('/api', getData)
